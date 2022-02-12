b70b7fb860fc11210c84e7995bce53bf0c515c65
alexmathew98 <alexmathewdev2099@gmail.com>
2022-02-13 00:39:40 +0400

    Updated changes

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,12 @@ class About extends Component {
                     <p className="mb-8 leading-relaxed">
                     I'm a motivated CS student with fundamental knowledge of software design, development and testing.I have a strong passion for building cool applications.Seeking to utilise my educational background with strong analytical,technical and programming skills to thrive as an entry level software developer.
                     </p>
+                    <h3 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        About me
+                    </h3>
+                    <p className="mb-8 leading-relaxed">
+                        I'm an international student born and raised in Abu Dhabi currently pursuing my bachelor's in Computer Science.While I'm not busy working on projects or assignments, I like to spend my time reading books, doing a little bit of stock trading, playing video games or playing soccer.I'm a huge fan of Hip-Hop and RnB music with some of my favourite artists being Drake,The Weeknd(huge XO fan),Future and PARTYNEXTDOOR. 
+                    </p>
                     </div>
                     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
